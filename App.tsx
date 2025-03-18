414b6a5cd5309af6c3a2cbbf7b2c913d1b9e6e09
Abhishek-yadav-12 <abhishekay2003@gmail.com>
2025-03-18 16:54:08 +0530

    Packages installed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import Clock from "./components/Clock";
+import { createStackNavigator, StackView } from "@react-navigation/stack";
+
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Clock />
     </View>
   );
 };
